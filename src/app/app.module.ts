4dfa8f277ff974713a7c792ff06bb8fac421199f
Alejandro <frozmiz@live.com>
2023-02-18 17:48:30 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './shared/components/nav/nav.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { HomeComponent } from './shared/components/home/home.component';
+import { RoomComponent } from './shared/components/room/room.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    RoomComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/components/room/room.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-room',
+  templateUrl: './room.component.html',
+  styleUrls: ['./room.component.css']
+})
+export class RoomComponent {
+
+}
