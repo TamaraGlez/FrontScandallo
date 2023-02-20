83a5f06535503c18faa4162ab9ea62787fb13dca
ednukros <isanmenarguez@gmail.com>
2023-02-20 17:02:58 +0100

    Feat: interface and get method

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,18 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Idefects } from '../interfaces/idefects';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  public url= 'http://escandallos-back.vercel.app/'
+  defect: Idefects = {
+
+    title: '',
+    image: '',
+    description: ''
+
+  }
+
+  id: number = 0;
+
+  public url = 'http://escandallos-back.vercel.app/'
   constructor(private http: HttpClient) { }
 
   getAllDefects() {
     return this.http.get(this.url + 'defects')
 
   }
+
+  getDefectsById(id: number) {
+    return this.http.get(this.url + id);
+
+  }
+
+
 
   // postDefects(defects: any) {
   //   return this.http.post('http://localhost:3100/defects', defects);
