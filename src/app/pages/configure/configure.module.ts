6efba14875311ea7315e03b8d1f8a3d1e29b972f
ednukros <isanmenarguez@gmail.com>
2023-02-21 20:29:20 +0100

    Fix: delete form at configure page

--- src/app/pages/configure/configure.module.ts
+++ src/app/pages/configure/configure.module.ts
@@ -3,13 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { ConfigureRoutingModule } from './configure-routing.module';
 import { ConfigureComponent } from './configure.component';
-import { FormProductComponent } from 'src/app/shared/components/form-product/form-product.component';
 
 
 @NgModule({
   declarations: [
     ConfigureComponent,
-    FormProductComponent
+  
   ],
   imports: [
     CommonModule,
