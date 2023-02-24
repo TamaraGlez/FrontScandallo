83a9aa92efe3ec5c03f4724f268c8f4c21f833d2
ednukros <isanmenarguez@gmail.com>
2023-02-24 17:18:54 +0100

    Style dashboard y nav

--- src/app/pages/catalogue/catalogue.module.ts
+++ src/app/pages/catalogue/catalogue.module.ts
@@ -4,13 +4,12 @@ import { CommonModule } from '@angular/common';
 import { CatalogueRoutingModule } from './catalogue-routing.module';
 import { CatalogueComponent } from './catalogue.component';
 import { FormsModule } from '@angular/forms';
-import { EscandalloFormEditComponent } from 'src/app/shared/components/escandallo-form-edit/escandallo-form-edit.component';
 
 
 @NgModule({
   declarations: [
     CatalogueComponent,
-    EscandalloFormEditComponent
+    
   ],
   imports: [
     CommonModule,
